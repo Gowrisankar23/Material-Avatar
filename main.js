441c671fd055c63dffef9ce264b759ee86a5370f
Gowrisankar <gowrisankarit15@gmail.com>
2017-06-28 17:24:35 +0530

    Added: Angular Material

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 (function () {
     'use strict';
 
-    var app = angular.module('SampleApp', ['materialAvatar']);
+    var app = angular.module('SampleApp', ['materialAvatar', 'ngMaterial']);
 
-    app.controller('MainController', function ($scope) {
+    app.controller('MainController', function ($scope, $compile) {
 
-        $scope.name = "Gowrisankar Anandhan";
+        // $scope.name = "Gowrisankar Anandhan";
+
+        $scope.createAvatar = function () {
+
+            angular.element(document.querySelector('#target')).empty();
+
+            var myEl = angular.element(document.querySelector('#target'));
+            myEl.append($compile("<ng-avatar name='" + $scope.name + "'></ng-avatar>")($scope));
+        }
     });
 } ());
